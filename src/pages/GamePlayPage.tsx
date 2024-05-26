b5222e40e2c9928da45e080f9dddbb0d5645fdc1
Bill Garrett <garrett@astralfrontier.org>
2024-05-26 15:37:45 -0700

    Gracefully handle text overflow in panels

--- src/pages/GamePlayPage.tsx
+++ src/pages/GamePlayPage.tsx
@@ -9,26 +9,28 @@ interface GamePlayPageProps {
   children?: ReactNode;
 }
 
+const LOREM = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Blandit turpis cursus in hac habitasse. Aliquet enim tortor at auctor urna nunc id cursus metus. Cras tincidunt lobortis feugiat vivamus. Nisl condimentum id venenatis a condimentum vitae. Mauris vitae ultricies leo integer malesuada nunc vel. Integer eget aliquet nibh praesent tristique. Dapibus ultrices in iaculis nunc. Dolor magna eget est lorem ipsum dolor. Augue neque gravida in fermentum et. Id venenatis a condimentum vitae sapien pellentesque habitant morbi. Mauris ultrices eros in cursus turpis. Sit amet luctus venenatis lectus magna fringilla urna porttitor rhoncus. Et ultrices neque ornare aenean euismod elementum nisi quis eleifend. Eu nisl nunc mi ipsum faucibus vitae aliquet nec. Risus at ultrices mi tempus. Faucibus in ornare quam viverra orci. Semper risus in hendrerit gravida rutrum quisque non.`;
+
 export default function GamePlayPage(props: GamePlayPageProps) {
   const ctx = useNakamaContext();
   const [cards, setCards] = useState<ReactNode[]>([]);
 
   const addCard = (_evt: any) => {
-    const cardText = prompt("Text for new card");
-    if (cardText) {
-      const node = <MarkdownCardWidget text={cardText} />;
+    const cardTitle = prompt("Text for new card");
+    if (cardTitle) {
+      const node = <MarkdownCardWidget title={cardTitle} text={LOREM} />;
       setCards((cards) => [...cards, node]);
     }
   };
 
   return (
     <NormalLayout>
-      <NakamaLoginRequired>
-        <button className="button is-primary" onClick={addCard}>
-          Add Card
-        </button>
-        <PanelContainerWidget>{cards}</PanelContainerWidget>
-      </NakamaLoginRequired>
+      {/* <NakamaLoginRequired> */}
+      <button className="button is-primary" onClick={addCard}>
+        Add Card
+      </button>
+      <PanelContainerWidget>{cards}</PanelContainerWidget>
+      {/* </NakamaLoginRequired> */}
     </NormalLayout>
   );
 }
--- src/widgets/MarkdownCardWidget.tsx
+++ src/widgets/MarkdownCardWidget.tsx
@@ -1,6 +1,7 @@
 import { ReactNode, useState } from "react";
 
 interface MarkdownCardWidgetProps {
+  title: string;
   text: string;
   children?: ReactNode;
 }
@@ -9,9 +10,12 @@ export default function MarkdownCardWidget(props: MarkdownCardWidgetProps) {
   const [cardText, setCardText] = useState<string>(props.text);
 
   return (
-    <div className="card" style={{ height: "100%" }}>
-      <div className="card-content">
-        <div className="content">{cardText}</div>
+    <div className="markdown-card-widget box is-flex is-flex-direction-column">
+      <div className="markdown-card-widget-title is-flex is-align-self-flex-start is-flex-grow-0">
+        {props.title}
+      </div>
+      <div className="markdown-card-widget-body is-flex is-align-self-flex-start is-flex-grow-0">
+        {cardText}
       </div>
     </div>
   );
