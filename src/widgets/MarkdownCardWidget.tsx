5347c3aff195bf39ffbfc46cdcc73506f717cbc0
Bill Garrett <garrett@astralfrontier.org>
2024-05-26 17:01:02 -0700

    feat: #3: Create panel container

--- src/widgets/MarkdownCardWidget.tsx
+++ src/widgets/MarkdownCardWidget.tsx
@@ -12,11 +12,21 @@ export default function MarkdownCardWidget(props: MarkdownCardWidgetProps) {
   return (
     <div className="markdown-card-widget box is-flex is-flex-direction-column">
       <div className="markdown-card-widget-title is-flex is-align-self-flex-start is-flex-grow-0">
-        {props.title}
-        <button
-          className="delete is-pulled-right"
-          onClick={props.deleteAction}
-        ></button>
+        <nav className="level">
+          <div className="level-left">
+            <div className="level-item">
+              <strong>{props.title}</strong>
+            </div>
+          </div>
+          <div className="level-right">
+            <div className="level-item">
+              <button
+                className="delete is-pulled-right"
+                onClick={props.deleteAction}
+              ></button>
+            </div>
+          </div>
+        </nav>
       </div>
       <div className="markdown-card-widget-body is-flex is-align-self-flex-start is-flex-grow-0">
         {props.text}
